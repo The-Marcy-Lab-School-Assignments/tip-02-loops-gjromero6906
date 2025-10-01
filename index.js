1a79b4729fdc3bf800a53d4921c6474475406a7a
gjromero6906 <gjr6906@gmail.com>
2025-10-01 14:03:31 -0400

    added and commented out solution if done reverse

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 // Write a function that prints the first n square numbers and also calculates the sum of these numbers.
 const printSquareNumbers = (n) => {
   //write code here
+  //original solution
   let total = 0;
   for (let i =1;i<=n;i++){
     total += i*i;
     console.log(i*i);
   }
   console.log(total);
+
+  //Reverse solution
+  // let total =0;
+  // let temp = 1;
+  // for(let i = n;i>0;i--){
+  //   total +=i+i;
+  //   console.log(temp*temp);
+  //   temp++;
+  // }
+
 }
 printSquareNumbers(5);
 //will print: 1, 4, 9, 16, 25
